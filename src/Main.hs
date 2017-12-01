7f8752a21a1f47576f88d8d773a61b0022d871fc
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-01 14:19:23 -0500

    solve 1_1

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = p1_1
+
+p1_1 :: IO ()
+p1_1 = do
+  raw <- readFile "input/1.txt"
+  let raw' = takeWhile (\x -> elem x "0123456789") raw
+  let closedLoop = [(head raw', last raw')]
+  let pairs = (zip raw' $ tail raw') ++ closedLoop
+  let pairs' = filter (\(x,y) -> x == y) pairs
+  let total = sum $ map (\(x,_) -> read [x]) pairs'
+  print $ total
