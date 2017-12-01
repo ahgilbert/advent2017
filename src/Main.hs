a4fb5544ec8183bd14235f90b2fdfa42d31a8c7f
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-01 14:28:09 -0500

    factor out shared logic, get same answer for p1_1

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,17 @@ module Main where
 main :: IO ()
 main = p1_1
 
+getInput1 = do
+  raw <- readFile "input/1.txt"
+  return $ takeWhile (\x -> elem x "0123456789") raw
+
+offsetBy offset source =
+  zip source $ (drop offset source) ++ (take offset source)
+
 p1_1 :: IO ()
 p1_1 = do
-  raw <- readFile "input/1.txt"
-  let raw' = takeWhile (\x -> elem x "0123456789") raw
-  let closedLoop = [(head raw', last raw')]
-  let pairs = (zip raw' $ tail raw') ++ closedLoop
+  raw <- getInput1
+  let pairs = offsetBy 1 raw
   let pairs' = filter (\(x,y) -> x == y) pairs
   let total = sum $ map (\(x,_) -> read [x]) pairs'
   print $ total
