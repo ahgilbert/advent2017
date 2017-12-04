8bd4511f760fdfa02a1bc36d1e8c1bc3bf720529
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-04 18:03:56 -0500

    solve P3_1

--- src/P3.hs
+++ src/P3.hs
@@ -3,25 +3,33 @@ module P3 where
 import Util
 import Data.List
 
-manhattan (a,b) (c,d) =
-  let dx = max a c - min a c
-      dy = max b d - min b d
-  in dx + dy
+getLayerStart target =
+  let ends = zip [0..] $ map (^ 2) [1,3..]
+      starts = ((0,0), 1) : map (\(i,m) -> ((i + 1, -i), m + 1)) ends
+      start = last $ takeWhile (\(_,m) -> m <= target) starts
+  in start
 
-getCoords n =
-  -- given spiral pattern, and setting 1 to (0,0), get (x,y) of n
-  let lowRights = map (\x -> x * x) $ map (\x -> x * 2 + 1) $ [0..]
-      diagonalLength = length $ tail $ takeWhile (<= n) $ lowRights
-      start = (diagonalLength, -diagonalLength)
-      sideLength = 1 + (2 * (diagonalLength + 1))
-  in (0,0)
+getSideLength x =
+  2 * x
 
---   76543
---   85432
---   96121
---   07890
---   12345
+proceed dist (ix,iy) =
+  let sideLength = getSideLength ix
+      right = ix
+      left = ix - sideLength
+      bottom = iy - 1
+      top = bottom + sideLength
+  in
+    if dist < sideLength
+    then (right, iy + dist)
+    else if dist < (2 * sideLength)
+    then (right - (dist - sideLength) - 1, top)
+    else if dist < (3 * sideLength)
+    then (left, top - 1 - (dist - (2 * sideLength)))
+    else (left + 1 + dist - (3 * sideLength), bottom)
 
 p3_1 = do
   input <- read <$> getInput 3
-  print $ input + 0
+  let layerStart = getLayerStart input
+      remainingDistance = input - (snd layerStart)
+  print $ manhattan (0,0) $ proceed remainingDistance (fst layerStart)
+  print ""
