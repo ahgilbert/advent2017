bd04c427199a7f2c97b01c2aa62b0c2a5fa58e81
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-07 11:26:43 -0500

    add array dependency, sketch p3_2

--- src/P3.hs
+++ src/P3.hs
@@ -2,6 +2,9 @@ module P3 where
 
 import Util
 import Data.List
+import Data.Array.IO
+
+type SpiralArray = IOArray (Int, Int) Int
 
 getLayerStart target =
   let ends = zip [0..] $ map (^ 2) [1,3..]
@@ -9,8 +12,7 @@ getLayerStart target =
       start = last $ takeWhile (\(_,m) -> m <= target) starts
   in start
 
-getSideLength x =
-  2 * x
+getSideLength x = 2 * x
 
 proceed dist (ix,iy) =
   let sideLength = getSideLength ix
@@ -33,3 +35,23 @@ p3_1 = do
       remainingDistance = input - (snd layerStart)
   print $ manhattan (0,0) $ proceed remainingDistance (fst layerStart)
   print ""
+
+add (a,b) (c,d) = (a+c, b+d)
+
+neighbors center =
+  let steps = [(x,y) | x <- [-1,0,1], y <- [-1,0,1]]
+  in map (add center) steps
+
+neighborhoodTotal :: SpiralArray -> (Int, Int) -> IO Int
+neighborhoodTotal array center =
+  sum <$> mapM (readArray array) (neighbors center)
+
+createArray :: Int -> IO SpiralArray
+createArray n = do
+  arr <- newArray ((-n, -n), (n, n)) 0 :: IO SpiralArray
+  return arr
+
+p3_2 = do
+  width <- (\(_,n) -> (n + 1) * 2) <$> getLayerStart <$> read <$> getInput 3
+  arr <- createArray width
+  return ()
