800b2d3e5db170f725101a1a7de9ad5a861150b3
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-09 16:42:34 -0500

    sketch p9 parsers

--- src/P9.hs
+++ src/P9.hs
@@ -15,12 +15,34 @@ p9_1 = do
   input <- Util.getInput 9
   print input
 
-parseNum :: Parser Int
-parseNum = do
+parseStream :: Parser P9Stream
+parseStream = do
   sign <- optional $ char '-'
   num <- some digitChar
   let n = read num :: Int
-  if (isJust sign)
-    then return ((-1) * n)
-    else return n
+  return Garbage
+
+parseGroup :: Parser P9Stream
+parseGroup = do
+  char '{'
+  contents <- many $ parseGroup <|> parseGarbage
+  char '}'
+  return $ Group contents
+
+parseGarbage :: Parser P9Stream
+parseGarbage = do
+  char '<'
+  many p9char
+  char '>'
+  return Garbage
+
+p9char = escapedChar <|> unescapedChar
+
+unescapedChar :: Parser Char
+unescapedChar = noneOf "!<"
+
+escapedChar :: Parser Char
+escapedChar = do
+  char '!'
+  anyChar
 
