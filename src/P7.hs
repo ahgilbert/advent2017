df903e24052321bc41055734712f18e4fd7ef9ca
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-10 21:21:16 -0500

    parse P7 input

--- src/P7.hs
+++ src/P7.hs
@@ -1,7 +1,10 @@
 module P7 where
 
 import Util
+import Control.Monad.State as S
 import Data.Either
+import Data.List
+import Data.List.Unique
 import Data.Maybe
 import Data.Tree
 import Data.Void
@@ -13,14 +16,26 @@ type Parser = Parsec Void String
 type Circuit = (String, Int, [String])
 
 p7 = do
-  input <- lines <$> Util.getInput 7
-  let parsed = map (runParser parseCircuit "") input
-  return ()
+  input <- lines <$> Util.getInput 1007
+  let parsed = rights $ map (runParser parseCircuit "") input
+      keys = map (\(x,_,_) -> x) parsed
+      descendants = unique $ concatMap (\(_,_,x) -> x) parsed
+      root = head $ filter (\k -> not $ elem k descendants) keys
+  -- put parsed
+  -- unfoldTreeM genTowerM root
+  print $ "ok"
+
+matchKey :: String -> Circuit -> Bool
+matchKey t (k,_,_) = k == t
+
+genTowerM :: String -> S.State [Circuit] (Circuit, [String])
+genTowerM =
+  undefined
 
 parseCircuit :: Parser Circuit
 parseCircuit = do
   key <- some letterChar
+  space
   weight <- read <$> between (char '(') (char ')') (some digitChar)
-  string " -> "
-  kids <- sepBy (some letterChar) (string ", ")
-  return (key, weight, kids)
+  kids <- optional $ space >> string "->" >> space >> sepBy (some letterChar) (string ", ")
+  return (key, weight, fromMaybe [] kids)
