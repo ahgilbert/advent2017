6fb4196d41428da222a34defca22e1153dd41fa6
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-12 07:59:56 -0500

    solve p12_2

--- src/P12.hs
+++ src/P12.hs
@@ -3,6 +3,7 @@ module P12 where
 import Util
 import Data.Either
 import Data.Graph
+import Data.List
 import Data.Maybe
 import Data.Void
 import Text.Megaparsec
@@ -12,9 +13,18 @@ p12 = do
   input <- lines <$> slurp 12
   let parsedInput = rights $ map (runParser parseNode "") input
       nodes = zipWith (\x (y,z) -> (x,y,z)) (repeat ()) parsedInput
+      keys = map (\(k,_) -> k) parsedInput
       (graph, faith, get) = graphFromEdges nodes
       p1 = reachable graph (fromJust $ get 0)
+      p2 = getGroups graph keys get
   print $ length p1
+  print $ length p2
+
+getGroups _ [] _ = []
+getGroups graph keys getter =
+  let seed = head keys
+      group = reachable graph (fromJust $ getter seed)
+  in group : (getGroups graph (keys \\ group) getter)
 
 parseNode :: Parser (Int, [Int])
 parseNode = do
