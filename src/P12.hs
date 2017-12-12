e511a14fe0485b52e33c9a46ec6c682560905a10
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-12 07:52:19 -0500

    solve 12_1

--- src/P12.hs
+++ src/P12.hs
@@ -1,17 +1,24 @@
 module P12 where
 
 import Util
+import Data.Either
+import Data.Graph
+import Data.Maybe
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
 p12 = do
   input <- lines <$> slurp 12
-  print "ok"
+  let parsedInput = rights $ map (runParser parseNode "") input
+      nodes = zipWith (\x (y,z) -> (x,y,z)) (repeat ()) parsedInput
+      (graph, faith, get) = graphFromEdges nodes
+      p1 = reachable graph (fromJust $ get 0)
+  print $ length p1
 
-parseNode :: Parser [(Int, Int)]
+parseNode :: Parser (Int, [Int])
 parseNode = do
   node <- num
   string " <-> "
   connected <- sepBy num (string ", ")
-  return $ map (\c -> (node, c)) connected
+  return $ (node, connected)
