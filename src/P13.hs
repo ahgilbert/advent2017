34d39fdd2a2e8fc77d348959927fbc4d960f2676
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-14 10:53:23 -0500

    solve 13_1 and 13_2

--- src/P13.hs
+++ src/P13.hs
@@ -10,17 +10,20 @@ testInput = [(0,3),(1,2),(4,4),(6,4)]
 p13 = do
   input <- lines <$> slurp 13
   let parsed = rights $ map (runParser parseFirewall "") input
-      naiveCost = tripCost parsed 0
-      allTripCosts = map (tripCost parsed) [0..]
+      naiveCost = scoreCollisions $ tripCollisions parsed 0
+      allTripCosts = map (tripCollisions parsed) [0..]
       bestDelay = length $ takeWhile (\x -> length x > 0) allTripCosts
   print naiveCost
   print bestDelay -- 1968 is too low
 
-tripCost guards delay =
-  map fst $ filter snd $ map (collision' delay) guards
+tripCollisions guards delay =
+  map fst $ filter snd $ map (detectCollision delay) guards
 
-collision' _ (d,1) = (d, True)
-collision' delay (d,r) = (d, 0 == mod (d + delay) (2 * (r - 1)))
+scoreCollisions collisions =
+  sum $ map (\(a,b) -> a * b) collisions
+
+detectCollision _ (d,1) = ((d,1), True)
+detectCollision delay (d,r) = ((d,r), 0 == mod (d + delay) (2 * (r - 1)))
 
 parseFirewall :: Parser (Int, Int)
 parseFirewall = do
