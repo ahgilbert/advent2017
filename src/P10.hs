42977dce7608758df5834d7d2501cbfea5e974a8
Alan Gilbert <alanhgilbert@gmail.com>
2017-12-14 11:49:25 -0500

    plausible, but wrong, 10_2

--- src/P10.hs
+++ src/P10.hs
@@ -1,9 +1,11 @@
 module P10 where
 
 import Util
+import Data.Bits
 import Data.Char
 import Data.List
 import Data.List.Split
+import Numeric (showHex)
 import System.IO.Unsafe
 
 p10size = 256
@@ -11,11 +13,11 @@ p10size = 256
 p10 = do
   input <- map read <$> splitOn "," <$> slurp 10 :: IO [Int]
   let initList = take p10size [0..]
-      step1 = oneStep input
+      step1 = knotHash input
       p10_1 = p1hash $ (\(x,_,_) -> x) $ step1
   print p10_1
 
-oneStep input = foldl step ([0..p10size-1], 0, 0) input
+knotHash input = foldl step ([0..p10size-1], 0, 0) input
 
 step :: ([a], Int, Int) -> Int -> ([a], Int, Int)
 step (list, start, skip) n =
@@ -39,12 +41,14 @@ step (list, start, skip) n =
 p1hash ls =
   (head ls) * (head $ tail ls)
 
-knotHash = undefined
+p10_2 = do
+  sparse <- (\(x,_,_) -> x) <$> knotHash <$> concat <$> replicate 64 <$> p2input
+  print $ sparseToDense sparse
 
 p2input = map (ord) <$> (\cs -> cs ++ "17,31,73,47,23") <$> slurp 10
 
-sparseToDense hs =
-  -- split into chunks of 16
-  -- map XOR_ALL to that
-  -- map dec_to_hex
-  undefined
+sparseToDense sparse =
+  let bits = chunksOf 16 sparse
+      xordBits = map (foldl xor 0) bits
+      hexHash = map (\i -> showHex i "") xordBits
+  in concat hexHash
